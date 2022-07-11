541cc1a151ae4964cc486eb413da7a32d3f6ad81
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-11 16:35:00 +0700

    2nd

---
+++ primemun.py
@@ -0,0 +1,14 @@
+# x=0
+# y=10
+# print("print number between",x, "and", y, "are:")
+# for num in range(x,y):
+#   if num >1 :
+#     for i in range (2,num):
+#       if (num%i)==0:
+#         break
+#       else:
+#         print(num)
+n=1
+while n <100:
+  print(n)
+  n=n+2
