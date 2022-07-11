e71bbb6e5f3ca2900948f597a61adfd3c2d8efb5
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-11 17:15:38 +0700

    1st

--- sort_num.py
+++ sort_num.py
@@ -1,4 +1,10 @@
-#sort function
-Numbers = [1,5,3,6,2,7,9,8,4]# list of number
-Numbers.sort()# use function sort to sort the number
-print(Numbers)
+# #sort function
+# Numbers = [1,5,3,6,2,7,9,8,4]# list of number
+# Numbers.sort()# use function sort to sort the number
+# print(Numbers)
+num = [1,5,3,6,2,7,9,8,4] 
+for i in range (len(num)):
+    for j in range(i+1 , len(num)):
+        if num[i]>num[j]: #< is from big to small , > is from small to big 
+            num[i],num[j]=num[j],num[i]
+print(num)
