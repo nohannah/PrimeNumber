931589266f19de0409160d8847a586848e912697
nohannah <67176677+nohannah@users.noreply.github.com>
2022-07-11 16:35:36 +0700

    2nd

--- primemun.py
+++ primemun.py
@@ -8,7 +8,11 @@
 #         break
 #       else:
 #         print(num)
-n=1
-while n <100:
-  print(n)
-  n=n+2
+
+# n=1
+# while n <100:
+#   print(n)
+#   n=n+2
+for num in range (100,200):
+  if all(num%i!=0 for i in range (2,num)):
+    print(num)
